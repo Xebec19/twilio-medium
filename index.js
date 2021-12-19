5097fc0397fb7f4c10321af405e55a7beb4312f8
xebec19 <rohant946@gmail.com>
2021-12-19 11:33:26 +0530

    send sms

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const dotenv = require("dotenv").config();
+if (dotenv.error) {
+  console.error("Error occurred while setting dot env files : ", dotenv.error);
+}
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const phoneNumber = process.env.TWILIO_PHONE_NUMBER;
+const client = require("twilio")(accountSid, authToken);
+
+app.get("/send-sms", (req, res) => {
+  client.messages
+    .create({ body: "Hi there", from: phoneNumber, to: "+917291003805" })
+    .then((message) => {
+      console.log(message.sid);
+      res.json({ sid: message.sid }).end();
+      return;
+    })
+    .catch((err) => {
+      res.json(error).end();
+      return;
+    });
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
